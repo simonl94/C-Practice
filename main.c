127de0de1209a6dbb3be0298971b7563de803a12
Simon Lee <slee1994@hotmail.com>
2023-04-30 11:35:55 +1000

    Initial commit

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
+// The classic FizzBuzz exercise
 #include <stdio.h>
 
 int main()
 {
-    printf("Hello, World");
+    for (int i = 0; i <= 100; i++)
+    {
+        if (i % 15 == 0)
+        {
+            printf("FizzBuzz\n");
+        }
+        else if ((i % 3) == 0)
+        {
+            printf("Fizz\n");
+        }
+        else if ((i % 5) == 0)
+        {
+            printf("Buzz\n");
+        }
+        else
+        {
+            printf("%d\n", i);
+        }
+    }
+
     return 0;
 }
